7a730712e557e93c268c492e9d10da8f5305706d
John Otte <johnsmall407@gmail.com>
2025-10-05 12:41:01 -0500

    functional

--- W06_Logo/src/vertex.glsl
+++ W06_Logo/src/vertex.glsl
@@ -3,16 +3,12 @@
 layout(location=0) in vec4 position;
 layout(location=1) in vec4 color;
 
-uniform float seconds;
+uniform mat4 rigid_matrix;
+uniform mat4 rotation_matrix;
 
 out vec4 vColor;
 
 void main() {
     vColor = color;
-    // Move from world coordinates to image coordinates (-1 to 1)
-    vec4 image_position = vec4(position.x / 3.0, position.y / 3.0, position.zw);
-    gl_Position = vec4(
-        image_position.xy*cos(seconds*0.6180339887498949),
-        image_position.zw
-    );
+    gl_Position = rigid_matrix * rotation_matrix * position;
 }
