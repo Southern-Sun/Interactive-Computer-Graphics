c692c04a96266bef93362105d4717d98c6f935cc
John Otte <johnsmall407@gmail.com>
2025-10-05 09:24:52 -0500

    basic geometry

--- W06_Logo/src/vertex.glsl
+++ W06_Logo/src/vertex.glsl
@@ -9,8 +9,10 @@ out vec4 vColor;
 
 void main() {
     vColor = color;
+    // Move from world coordinates to image coordinates (-1 to 1)
+    vec4 image_position = vec4(position.x / 3.0, position.y / 3.0, position.zw);
     gl_Position = vec4(
-        position.xy*cos(seconds*0.6180339887498949),
-        position.zw
+        image_position.xy*cos(seconds*0.6180339887498949),
+        image_position.zw
     );
 }
