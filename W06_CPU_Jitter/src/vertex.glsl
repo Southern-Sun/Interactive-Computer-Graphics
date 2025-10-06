e0096f8013d4bf4164ca4cc8ada7feb4a8d1d4c3
John Otte <johnsmall407@gmail.com>
2025-10-05 19:29:58 -0500

    copy over

---
+++ W06_CPU_Jitter/src/vertex.glsl
@@ -0,0 +1,14 @@
+#version 300 es
+
+layout(location=0) in vec4 position;
+layout(location=1) in vec4 color;
+
+uniform mat4 rigid_matrix;
+uniform mat4 rotation_matrix;
+
+out vec4 vColor;
+
+void main() {
+    vColor = color;
+    gl_Position = rigid_matrix * rotation_matrix * position;
+}
