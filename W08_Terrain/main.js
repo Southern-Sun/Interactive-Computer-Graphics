74bc5c9097ba53ac92dc1d32472a1daf930ac58b
John Otte <johnsmall407@gmail.com>
2025-10-08 10:30:57 -0500

    copy forward code

---
+++ W08_Terrain/main.js
@@ -0,0 +1,364 @@
+
+const IlliniBlue = new Float32Array([0.075, 0.16, 0.292, 1])
+const IlliniOrange = new Float32Array([1, 0.373, 0.02, 1])
+const IdentityMatrix = new Float32Array([1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1])
+
+/**
+ * Given the source code of a vertex and fragment shader, compiles them,
+ * and returns the linked program.
+ */
+function compileShader(vs_source, fs_source) {
+    const vs = gl.createShader(gl.VERTEX_SHADER)
+    gl.shaderSource(vs, vs_source)
+    gl.compileShader(vs)
+    if (!gl.getShaderParameter(vs, gl.COMPILE_STATUS)) {
+        console.error(gl.getShaderInfoLog(vs))
+        throw Error("Vertex shader compilation failed")
+    }
+
+    const fs = gl.createShader(gl.FRAGMENT_SHADER)
+    gl.shaderSource(fs, fs_source)
+    gl.compileShader(fs)
+    if (!gl.getShaderParameter(fs, gl.COMPILE_STATUS)) {
+        console.error(gl.getShaderInfoLog(fs))
+        throw Error("Fragment shader compilation failed")
+    }
+
+    const program = gl.createProgram()
+    gl.attachShader(program, vs)
+    gl.attachShader(program, fs)
+    gl.linkProgram(program)
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        console.error(gl.getProgramInfoLog(program))
+        throw Error("Linking failed")
+    }
+    
+    const uniforms = {}
+    for(let i=0; i<gl.getProgramParameter(program, gl.ACTIVE_UNIFORMS); i+=1) {
+        let info = gl.getActiveUniform(program, i)
+        uniforms[info.name] = gl.getUniformLocation(program, info.name)
+    }
+    program.uniforms = uniforms
+
+    return program
+}
+
+/**
+ * Sends per-vertex data to the GPU and connects it to a VS input
+ * 
+ * @param data    a 2D array of per-vertex data (e.g. [[x,y,z,w],[x,y,z,w],...])
+ * @param loc     the layout location of the vertex shader's `in` attribute
+ * @param mode    (optional) gl.STATIC_DRAW, gl.DYNAMIC_DRAW, etc
+ * 
+ * @returns the ID of the buffer in GPU memory; useful for changing data later
+ */
+function supplyDataBuffer(data, loc, mode) {
+    if (mode === undefined) mode = gl.STATIC_DRAW
+    
+    const buf = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, buf)
+    const f32 = new Float32Array(data.flat())
+    gl.bufferData(gl.ARRAY_BUFFER, f32, mode)
+    
+    gl.vertexAttribPointer(loc, data[0].length, gl.FLOAT, false, 0, 0)
+    gl.enableVertexAttribArray(loc)
+    
+    return buf;
+}
+
+/**
+ * Creates a Vertex Array Object and puts into it all of the data in the given
+ * JSON structure, which should have the following form:
+ * 
+ * ````
+ * {"triangles": a list of of indices of vertices
+ * ,"attributes":
+ *  [ a list of 1-, 2-, 3-, or 4-vectors, one per vertex to go in location 0
+ *  , a list of 1-, 2-, 3-, or 4-vectors, one per vertex to go in location 1
+ *  , ...
+ *  ]
+ * }
+ * ````
+ * 
+ * @returns an object with four keys:
+ *  - mode = the 1st argument for gl.drawElements
+ *  - count = the 2nd argument for gl.drawElements
+ *  - type = the 3rd argument for gl.drawElements
+ *  - vao = the vertex array object for use with gl.bindVertexArray
+ */
+function setup_geometry(geometry) {
+    var triangleArray = gl.createVertexArray()
+    gl.bindVertexArray(triangleArray)
+
+    for(let i=0; i<geometry.attributes.length; i+=1) {
+        let data = geometry.attributes[i]
+        supplyDataBuffer(data, i)
+    }
+
+    var indices = new Uint16Array(geometry.triangles.flat())
+    var indexBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer)
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW)
+
+    return {
+        mode: gl.TRIANGLES,
+        count: indices.length,
+        type: gl.UNSIGNED_SHORT,
+        vao: triangleArray
+    }
+}
+
+function makeGeom() {
+    g = {"triangles":
+        []
+    ,"attributes":
+        [ // position
+            []
+        , // color
+            []
+        ]
+    }
+    g.attributes[0].push([0,0,0])
+    g.attributes[1].push([1,1,1])
+    const n = 24
+    const r = 3
+    for(let i=0; i<n; i+=1) {
+        let ang = i*(2*Math.PI)/n
+        let r2 = r - (i%2)
+        g.attributes[0].push(
+            [Math.cos(ang)*r2,0,Math.sin(ang)*r2],
+        )
+        g.attributes[1].push([0,(i%2),0])
+    }
+    for(let i=0; i<n-1; i+=1) {
+        g.triangles.push(0, i+1, i+2)
+    }
+    g.triangles.push(0, n, 1)
+
+    return g
+}
+
+/** Converts information about orbital & spin periods in seconds to frequencies we can use
+ * for rotation
+ */
+function period(period_in_seconds) {
+    if (period_in_seconds == 0) {
+        return 0
+    }
+    return 2 * Math.PI / period_in_seconds
+}
+
+
+/** Draw one frame */
+function draw(seconds) {
+    gl.clearColor(...IlliniBlue) // f(...[1,2,3]) means f(1,2,3)
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+    gl.useProgram(program)
+
+    // gl.bindVertexArray(tetrahedron.vao)
+
+    // gl.uniform4fv(program.uniforms.color, IlliniOrange)
+    // let m = m4rotX(seconds)
+    // let v = m4view([Math.cos(seconds/2),2,3], [0,0,0], [0,1,0])
+    // gl.uniformMatrix4fv(program.uniforms.mv, false, m4mul(v,m))
+    // gl.uniformMatrix4fv(program.uniforms.p, false, p)
+    
+    // gl.drawElements(tetrahedron.mode, tetrahedron.count, tetrahedron.type, 0)
+
+    // let wrapped = seconds % 4
+    // let stage = Math.floor(wrapped)
+    // let t = wrapped - stage
+    // if (stage == 0) {
+    //     tr = m4trans(1,0,1*(1-t) + -1*(t))
+    // } else if (stage == 1) { 
+    //     tr = m4trans(...lerp(t, [1,0,-1], [-1,0,-1]))
+    // } else if (stage == 2) { 
+    //     tr = m4trans(...lerp(t, [-1,0,-1], [-1,0,1]))
+    // } else { // stage == 3
+    //     tr = m4trans(...lerp(t, [-1,0,1], [1,0,1]))
+    // }
+
+    // let m2 = m4mul(tr, m4scale(0.5, 0.5, 0.5))
+    // gl.uniformMatrix4fv(program.uniforms.mv, false, m4mul(v,m2))
+    // gl.drawElements(tetrahedron.mode, tetrahedron.count, tetrahedron.type, 0)
+
+    // let m3 = m4mul(m, m4trans(0,1,0), m4scale(0.5, 0.5, 0.5), m4rotZ(seconds))
+    // gl.uniformMatrix4fv(program.uniforms.mv, false, m4mul(v,m3))
+    // gl.drawElements(tetrahedron.mode, tetrahedron.count, tetrahedron.type, 0)
+
+    // let m4 = m4mul(m3, m4trans(1,0,0), m4scale(0.5, 0.5, 0.5))
+    // gl.uniformMatrix4fv(program.uniforms.mv, false, m4mul(v,m4))
+    // gl.drawElements(tetrahedron.mode, tetrahedron.count, tetrahedron.type, 0)
+
+    // gl.bindVertexArray(octahedron.vao)
+    // gl.uniformMatrix4fv(program.uniforms.mv, false, v)
+    // gl.drawElements(octahedron.mode, octahedron.count, octahedron.type, 0)
+
+    // Slow down the animation here
+    seconds = seconds / 1
+
+    // Setup our parameters -- Spins & Orbits are periods in seconds
+    const SUN_SPIN = 2
+
+    const EARTH_SPIN = 0.4
+    const EARTH_ORBIT = 4
+    const EARTH_DISTANCE = 4
+    const EARTH_SCALE = .5
+
+    const MARS_SPIN = EARTH_SPIN * 2.2
+    const MARS_ORBIT = EARTH_ORBIT * 1.9
+    const MARS_DISTANCE = EARTH_DISTANCE * 1.6
+    const MARS_SCALE = .4
+
+    // This is an "anti-spin" -- reduce the orbit speed proportional to the spin its inheriting
+    // When we orbit, we are tidally locked by default, so do not add spin
+    const MOON_ORBIT = -EARTH_SPIN / .90
+    const MOON_SPIN = 0
+    const MOON_DISTANCE = 2.5
+    const MOON_SCALE = .5
+
+    // We are adding spin to Phobos's orbit, which already contains 1 Mars Spin
+    const PHOBOS_ORBIT = MARS_SPIN / 2
+    const PHOBOS_SPIN = 0
+    const PHOBOS_DISTANCE = 2.5
+    const PHOBOS_SCALE = .5
+
+    // Deimos only needs to go a little faster, so add a fairly large period to the Mars Spin
+    const DEIMOS_ORBIT = 4
+    const DEIMOS_SPIN = 0
+    const DEIMOS_DISTANCE = PHOBOS_DISTANCE * 2
+    const DEIMOS_SCALE = PHOBOS_SCALE / 2
+
+
+    // Set our perspective matrix
+    gl.uniformMatrix4fv(program.uniforms.perspective, false, perspective_matrix)
+    var view_matrix = m4view([1, 5, 11], [0, 0, 0], [0, 1, 0])
+
+    // Render our planets (octahedrons) in hierarchal order
+    gl.bindVertexArray(octahedron.vao)
+
+    // A large octahedron ("the Sun")
+    //     fixed at the origin
+    //     spinning a full rotation once every two seconds
+    sun_model = m4rotY(period(SUN_SPIN) * seconds)
+    gl.uniformMatrix4fv(program.uniforms.model_view, false, m4mul(view_matrix, sun_model))
+    gl.drawElements(octahedron.mode, octahedron.count, octahedron.type, 0)
+
+    // A smaller octahedron ("the Earth")
+    //     orbiting the Sun once every few seconds
+    //     spinning like a top several times a second
+    // Ignore the sun model since we don't need to inherit any of its movement -- it's easier just
+    // to use a straight rotation then a relative one.
+    earth_model = m4mul(
+        m4rotY(period(EARTH_ORBIT) * seconds),
+        m4trans(EARTH_DISTANCE, 0, 0),
+        m4rotY(period(EARTH_SPIN) * seconds),
+        m4scale(EARTH_SCALE, EARTH_SCALE, EARTH_SCALE)
+    )
+    gl.uniformMatrix4fv(program.uniforms.model_view, false, m4mul(view_matrix, earth_model))
+    gl.drawElements(octahedron.mode, octahedron.count, octahedron.type, 0)
+
+    // An octahedron ("Mars") a little smaller than the Earth
+    //     1.6 times as far from the Sun as the Earth
+    //     orbiting the Sun 1.9 times slower than the Earth
+    //     spinning like a top 2.2 times slower than the Earth
+    // Again not inheriting either the earth or sun models
+    mars_model = m4mul(
+        m4rotY(period(MARS_ORBIT) * seconds),
+        m4trans(MARS_DISTANCE, 0, 0),
+        m4rotY(period(MARS_SPIN) * seconds),
+        m4scale(MARS_SCALE, MARS_SCALE, MARS_SCALE)
+    )
+    gl.uniformMatrix4fv(program.uniforms.model_view, false, m4mul(view_matrix, mars_model))
+    gl.drawElements(octahedron.mode, octahedron.count, octahedron.type, 0)
+
+    // Render our moons (tetrahedrons) in hierarchal order
+    gl.bindVertexArray(tetrahedron.vao)
+
+    // A tetrahedron ("the Moon") smaller than the Earth
+    //     smaller than the Earth
+    //     orbiting the Earth faster than the Earth orbits the Sun but slower than the Earth spins
+    //     always presenting the same side of itself to the Earth
+    moon_model = m4mul(
+        earth_model,
+        m4rotY(period(MOON_ORBIT) * seconds),
+        m4trans(MOON_DISTANCE, 0, 0),
+        m4rotY(period(MOON_SPIN) * seconds),
+        m4scale(MOON_SCALE, MOON_SCALE, MOON_SCALE)
+    )
+    gl.uniformMatrix4fv(program.uniforms.model_view, false, m4mul(view_matrix, moon_model))
+    gl.drawElements(tetrahedron.mode, tetrahedron.count, tetrahedron.type, 0)
+
+    // A tetrahedron ("Phobos") smaller than Mars
+    //     orbiting Mars several times faster than Mars spins
+    //     always presenting the same side of itself to Mars
+    phobos_model = m4mul(
+        mars_model,
+        m4rotY(period(PHOBOS_ORBIT) * seconds),
+        m4trans(PHOBOS_DISTANCE, 0, 0),
+        m4rotY(period(PHOBOS_SPIN) * seconds),
+        m4scale(PHOBOS_SCALE, PHOBOS_SCALE, PHOBOS_SCALE)
+    )
+    gl.uniformMatrix4fv(program.uniforms.model_view, false, m4mul(view_matrix, phobos_model))
+    gl.drawElements(tetrahedron.mode, tetrahedron.count, tetrahedron.type, 0)
+
+    
+    // A tetrahedron ("Deimos") half the size of Phobos
+    //     twice as far from Mars as Phobos
+    //     orbiting Mars only a little faster than Mars spins
+    //     always presenting the same side of itself to Mars
+    deimos_model = m4mul(
+        mars_model,
+        m4rotY(period(DEIMOS_ORBIT) * seconds),
+        m4trans(DEIMOS_DISTANCE, 0, 0),
+        m4rotY(period(DEIMOS_SPIN) * seconds),
+        m4scale(DEIMOS_SCALE, DEIMOS_SCALE, DEIMOS_SCALE)
+    )
+    gl.uniformMatrix4fv(program.uniforms.model_view, false, m4mul(view_matrix, deimos_model))
+    gl.drawElements(tetrahedron.mode, tetrahedron.count, tetrahedron.type, 0)
+
+}
+
+/** Compute any time-varying or animated aspects of the scene */
+function tick(milliseconds) {
+    let seconds = milliseconds / 1000;
+
+    draw(seconds)
+    requestAnimationFrame(tick)
+}
+
+/** Resizes the canvas to completely fill the screen */
+function fillScreen() {
+    let canvas = document.querySelector('canvas')
+    document.body.style.margin = '0'
+    canvas.style.width = '100vw'
+    canvas.style.height = '100vh'
+    canvas.width = canvas.clientWidth
+    canvas.height = canvas.clientHeight
+    canvas.style.width = ''
+    canvas.style.height = ''
+    if (window.gl) {
+        gl.viewport(0,0, canvas.width, canvas.height)
+        window.perspective_matrix = m4perspNegZ(1, 20, 1.5, canvas.width, canvas.height)
+    }
+}
+
+/** Compile, link, set up geometry */
+window.addEventListener('load', async (event) => {
+    window.gl = document.querySelector('canvas').getContext('webgl2',
+        // optional configuration object: see https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext
+        {antialias: false, depth:true, preserveDrawingBuffer:true}
+    )
+    let vs = await fetch('src/vertex.glsl').then(res => res.text())
+    let fs = await fetch('src/fragment.glsl').then(res => res.text())
+    window.program = compileShader(vs,fs)
+    gl.enable(gl.DEPTH_TEST)
+
+    let data = await fetch('assets/geometry.json').then(r=>r.json())
+    window.tetrahedron = setup_geometry(data.tetrahedron)
+    window.octahedron = setup_geometry(data.octahedron)
+    
+    fillScreen()
+    window.addEventListener('resize', fillScreen)
+    requestAnimationFrame(tick)
+})
